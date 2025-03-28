b60a53fbec1b422fc5ba818c1425fef6e3785a52
Leyla Memiguven <leyla.memiguven@mail.utoronto.ca>
2025-03-28 14:09:07 -0400

    Adding styling changes for the hover and background color changing section

--- index.js
+++ index.js
@@ -25,3 +25,39 @@
   showImage(current);
   startCarousel();
 
+
+const s2Img1 = document.querySelector('.s2-img1');
+const section2 = document.querySelector('.section2');
+
+s2Img1.addEventListener('mouseenter', () => {
+  section2.classList.add('highlight-bg-purple');
+});
+
+s2Img1.addEventListener('mouseleave', () => {
+  section2.classList.remove('highlight-bg-purple');
+});
+
+
+const s2Img2 = document.querySelector('.s2-img2');
+
+s2Img2.addEventListener('mouseenter', () => {
+  section2.classList.add('highlight-bg-pink');
+});
+
+s2Img2.addEventListener('mouseleave', () => {
+  section2.classList.remove('highlight-bg-pink');
+});
+
+
+const s2Img3 = document.querySelector('.s2-img3');
+
+s2Img3.addEventListener('mouseenter', () => {
+  section2.classList.add('highlight-bg-orange');
+});
+
+s2Img3.addEventListener('mouseleave', () => {
+  section2.classList.remove('highlight-bg-orange');
+});
+
+
+
